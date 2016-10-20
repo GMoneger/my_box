7162ba3514350428101d5e97306861f59c6af621
gmo <gmo@emakina.fr>
2016-10-20 15:37:12 +0200

    Connexion / Déconnexion / Message

--- src/ChatBundle/Resources/public/js/app.js
+++ src/ChatBundle/Resources/public/js/app.js
@@ -12,19 +12,55 @@ var server = http.createServer(function(req, res) {
 // Chargement de socket.io
 var io = require('socket.io').listen(server);
 
+var listUsers = [] ;
+
 // Quand un client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket) {
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
     socket.on('username', function(pseudo) {
-        socket.pseudo = pseudo;
+        var ID = Math.random().toString(16).slice(5);
+        listUsers.push({
+            "socket" : socket,
+            "pseudo" : pseudo,
+            "ID": ID
+        });
+
+        console.log(listUsers) ;
+
         socket.broadcast.emit('username', pseudo);
     });
 
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+        var userIndice = getUser(socket) ;
+
+        console.log(message, userIndice) ;
+
+        socket.broadcast.emit('message', {pseudo: listUsers[userIndice]['pseudo'], message: message});
+    });
+
+    socket.on('disconnect', function() {
+        var userIndice = getUser(socket) ;
+        userPseudo = listUsers[userIndice]['pseudo'] ;
+        console.log(userPseudo + ' Disconnect') ;
+
+        console.log(listUsers) ;
+        listUsers.splice(userIndice, 1);
+        console.log(listUsers) ;
+
+        socket.broadcast.emit('dc', userPseudo);
     });
 
 });
+
+function getUser(socket) {
+    var l = listUsers.length;
+    for (var i=0 ; i<l ; i++) {
+        if (listUsers[i]['socket'] == socket) {
+            return i;
+        }
+    }
+    return -1;
+}
 
 server.listen(8080);
