ae3e52e1e7a1c5c9ac458426c5694999203fbae6
gmo <gmo@emakina.fr>
2016-12-22 11:33:40 +0100

    template

--- src/CinemaBundle/Controller/DefaultController.php
+++ src/CinemaBundle/Controller/DefaultController.php
@@ -5,13 +5,40 @@ namespace CinemaBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * @Route("/cinema")
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/cinema")
+     * @Route("/")
      */
     public function indexAction()
     {
         return $this->render('CinemaBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/room1")
+     */
+    public function room1Action()
+    {
+        return $this->render('CinemaBundle:Default/rooms:room1.html.twig');
+    }
+
+    /**
+     * @Route("/room2")
+     */
+    public function room2Action()
+    {
+        return $this->render('CinemaBundle:Default/rooms:room2.html.twig');
+    }
+
+    /**
+     * @Route("/room3")
+     */
+    public function room3Action()
+    {
+        return $this->render('CinemaBundle:Default/rooms:room3.html.twig');
     }
 }
